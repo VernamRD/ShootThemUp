db15e512f64071900a5d93bc386185aeceea3816
VernamRD <vernamrdcross@mail.ru>
2021-11-10 22:17:55 +0300

    Create STUCharacterMovementComponent.cpp
    Was created to realize fast running

---
+++ Source/ShootThemUp/Private/Components/STUCharacterMovementComponent.cpp
@@ -0,0 +1,11 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#include "Components/STUCharacterMovementComponent.h"
+#include "Player/STUBaseCharacter.h"
+
+float USTUCharacterMovementComponent::GetMaxSpeed() const
+{
+    const float MaxSpeed = Super::GetMaxSpeed();
+    const ASTUBaseCharacter* Player = Cast<ASTUBaseCharacter>(GetPawnOwner());
+    return Player && Player->IsRunning() ? MaxSpeed * RunModifier : MaxSpeed;
+}
