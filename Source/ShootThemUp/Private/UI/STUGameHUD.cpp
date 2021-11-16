e7dc98c5da86bd82747ad553663f43c4f686b4ae
VernamRD <vernamrdcross@mail.ru>
2021-11-16 14:45:31 +0300

    Test dev crosshair

---
+++ Source/ShootThemUp/Private/UI/STUGameHUD.cpp
@@ -0,0 +1,23 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#include "UI/STUGameHUD.h"
+#include "Engine/Canvas.h"
+
+void ASTUGameHUD::DrawHUD()
+{
+    Super::DrawHUD();
+
+    DrawCrossHair();
+}
+
+void ASTUGameHUD::DrawCrossHair() 
+{
+    const TInterval<float> Center(Canvas->SizeX * 0.5f, Canvas->SizeY * 0.5f);
+
+    const float HalfLineSize = 10.0f;
+    const float LineThickness = 2.0f;
+    const FLinearColor LineColor = FLinearColor::Green;
+
+    DrawLine(Center.Min - HalfLineSize, Center.Max, Center.Min + HalfLineSize, Center.Max, LineColor, LineThickness);
+    DrawLine(Center.Min, Center.Max - HalfLineSize, Center.Min, Center.Max + HalfLineSize, LineColor, LineThickness);
+}
---
+++ Source/ShootThemUp/Public/UI/STUGameHUD.h
@@ -0,0 +1,22 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "STUGameHUD.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUGameHUD : public AHUD
+{
+    GENERATED_BODY()
+
+public:
+    virtual void DrawHUD() override;
+
+private:
+    void DrawCrossHair();
+};
