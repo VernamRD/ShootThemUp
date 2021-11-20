38480ace9bc889e6af41873bd8e4478b8bca2ee3
VernamRD <vernamrdcross@mail.ru>
2021-11-20 19:15:03 +0300

    Launcer impact FX

--- Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
+++ Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
@@ -56,7 +56,7 @@ void ASTUProjectile::OnProjectileHit(
         GetController(),                               //
         DoFullDamage);
 
-    DrawDebugSphere(GetWorld(), GetActorLocation(), DamageRadius, 24, FColor::Red, false, 5.0f);
+    //DrawDebugSphere(GetWorld(), GetActorLocation(), DamageRadius, 24, FColor::Red, false, 5.0f);
     WeaponFXComponent->PlayImpactFX(Hit);
 
     Destroy();
